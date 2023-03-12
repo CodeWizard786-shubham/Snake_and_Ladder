4c1ac608c205c7e1b1d8813c53361a41f79b7093
shubham shirke <shirkeshubham84@gmail.com>
2023-03-12 19:53:27 +0530

    Added:Program that rolls the dice randomly until goal position

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -5,23 +5,29 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
-        int cont=1;
+        int goal = 100;
         int playerPosition = 0;
         int[] board = new int[101];
         int[] snakes = {17, 54, 62, 64, 87, 93, 95, 98};
         int[] ladders = {4, 9, 20, 28, 40, 51, 63, 71};
-        System.out.println("Player Position:"+playerPosition);
+        System.out.println("Player Position:" + playerPosition);
         System.out.println("Press S to Roll the dice:");
         char p = sc.next().charAt(0);
-        switch (p){
+        switch (p) {
             case 's':
-                int dice_roll = rand.nextInt(6) + 1;
-                playerPosition = playerPosition + dice_roll;
-                System.out.println("Player Position:" + playerPosition);
+                while (playerPosition!=goal) {
+                    int dice_roll = rand.nextInt(6) + 1;
+                    playerPosition = playerPosition + dice_roll;
+                    System.out.println("Player Position:" + playerPosition);
+                }
                 break;
             default:
                 System.out.println("Entered Wrong character");
         }
+        if(playerPosition==100){
+            System.out.println("Player won");
 
+        }
+        }
     }
-}
+
