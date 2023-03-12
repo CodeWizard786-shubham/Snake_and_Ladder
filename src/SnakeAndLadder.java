9d62eb1b159f064daaa3c3baa01c7bb5dfe22671
shubham shirke <shirkeshubham84@gmail.com>
2023-03-12 21:01:49 +0530

    Added:check condition to ensure player gets winning position exact 100

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -16,23 +16,35 @@ public class SnakeAndLadder {
                 while (playerPosition!=goal) {
                     int option = rand.nextInt(3) + 1;
                     int dice_roll = rand.nextInt(6) + 1;
+                    System.out.println("Number on Dice:"+dice_roll);
                     dice_count++;
                     if(option==1){
                         playerPosition=playerPosition+0;
+                        System.out.println("No Play");
                         System.out.println("Player Position:" + playerPosition);
                     }
                     else if(option==2){
                     playerPosition = playerPosition + dice_roll;
+                    System.out.println("Congrats you found a Ladder");
                     System.out.println("Player Position:" + playerPosition);
-                }
+                    }
                     else{
                         playerPosition = playerPosition - dice_roll;
                         if(playerPosition<1){
                             playerPosition=0;
                         }
+                        System.out.println("sorry Snake bite");
                         System.out.println("Player Position:" + playerPosition);
                         }
-                    if(playerPosition==100){
+                    if(playerPosition>=100){
+                        playerPosition = playerPosition - dice_roll;
+                        while (playerPosition!=100) {
+                            int dice_roll1 = rand.nextInt(6) + 1;
+                            playerPosition=playerPosition+dice_roll1;
+                            if(playerPosition==100){
+                                break;
+                            }
+                        }
                         break;
 
                     }
